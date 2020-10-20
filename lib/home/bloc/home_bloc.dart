be087e4d9e1478b115a2f777a9d19da71430e261
01fc <javier.francisco01.09@gmail.com>
2020-10-19 23:14:16 -0600

    agregado ui con bloc falta: logica para subir y descargar elementos a firebase

---
+++ lib/home/bloc/home_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial());
+
+  @override
+  Stream<HomeState> mapEventToState(
+    HomeEvent event,
+  ) async* {}
+}
